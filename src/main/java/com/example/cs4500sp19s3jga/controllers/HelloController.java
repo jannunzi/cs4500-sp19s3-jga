817914f6547203caaf4c01dc779fc9e2f1110652
jannunzi <jannunzi@gmail.com>
2019-01-11 16:14:47 -0500

    added get username

--- src/main/java/com/example/cs4500sp19s3jga/controllers/HelloController.java
+++ src/main/java/com/example/cs4500sp19s3jga/controllers/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.cs4500sp19s3jga.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.cs4500sp19s3jga.models.User;
@@ -17,6 +18,15 @@ public class HelloController {
 		alice.setUsername("alice");
 		alice.setFirstName("Alice");
 		alice.setLastName("Wonderland");
+		return alice;
+	}
+	@GetMapping("/api/user/{username}")
+	public User getUser(
+			@PathVariable("username") String u) {
+		User alice = new User();
+		alice.setUsername(u);
+		alice.setFirstName("Somebody");
+		alice.setLastName("Someone else");
 		return alice;
 	}
 }
