d80c3d3be3a2427272246bb374f9f58197736b32
jannunzi <jannunzi@gmail.com>
2019-01-11 16:11:15 -0500

    added get user alice

--- src/main/java/com/example/cs4500sp19s3jga/controllers/HelloController.java
+++ src/main/java/com/example/cs4500sp19s3jga/controllers/HelloController.java
@@ -3,10 +3,20 @@ package com.example.cs4500sp19s3jga.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.cs4500sp19s3jga.models.User;
+
 @RestController
 public class HelloController {
 	@GetMapping("/api/hello")
 	public String sayHello() {
 		return "Hello World";
+	}
+	@GetMapping("/api/user/alice")
+	public User getUser() {
+		User alice = new User();
+		alice.setUsername("alice");
+		alice.setFirstName("Alice");
+		alice.setLastName("Wonderland");
+		return alice;
 	}
 }
