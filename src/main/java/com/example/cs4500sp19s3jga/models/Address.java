94e49e38c181ddb3f309917301b96b74e51bebb9
jannunzi <jannunzi@gmail.com>
2019-01-27 19:03:22 -0500

    added address class

---
+++ src/main/java/com/example/cs4500sp19s3jga/models/Address.java
@@ -0,0 +1,56 @@
+package com.example.cs4500sp19s3jga.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="addresses")
+public class Address {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String street1;
+	private String street2;
+	private String city;
+	private String state;
+	private String country;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getStreet1() {
+		return street1;
+	}
+	public void setStreet1(String street1) {
+		this.street1 = street1;
+	}
+	public String getStreet2() {
+		return street2;
+	}
+	public void setStreet2(String street2) {
+		this.street2 = street2;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+}
