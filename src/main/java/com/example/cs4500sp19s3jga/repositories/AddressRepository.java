7c9984112fed5425f49ce63d12d52f02bab14ab4
jannunzi <jannunzi@gmail.com>
2019-01-27 19:50:41 -0500

    added find address by id and find addresses for user

--- src/main/java/com/example/cs4500sp19s3jga/repositories/AddressRepository.java
+++ src/main/java/com/example/cs4500sp19s3jga/repositories/AddressRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.example.cs4500sp19s3jga.models.Address;
 
@@ -11,4 +12,7 @@ public interface AddressRepository
 	extends CrudRepository<Address, Integer>{
 	@Query(value="SELECT address FROM Address address")
 	public List<Address> findAllAddresses();
+	@Query(value="SELECT address FROM Address address WHERE address.id=:id")
+	public Address findAddressById(
+			@Param("id") Integer id);
 }
