0b95d0b5f5b5f9bb9c66ce6e8e2da8a69a2bd111
jannunzi <jannunzi@gmail.com>
2019-01-27 19:51:30 -0500

    added find address by id and find addresses for user REST

--- src/main/java/com/example/cs4500sp19s3jga/services/AddressService.java
+++ src/main/java/com/example/cs4500sp19s3jga/services/AddressService.java
@@ -4,17 +4,31 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.cs4500sp19s3jga.models.Address;
 import com.example.cs4500sp19s3jga.repositories.AddressRepository;
+import com.example.cs4500sp19s3jga.repositories.UserRepository;
 
 @RestController
 public class AddressService {
 	@Autowired
 	AddressRepository addressRepository;
+	@Autowired
+	UserRepository userRepository;
+	@GetMapping("/api/users/{userId}/addresses")
+	public List<Address> findAddressesForUser(
+			@PathVariable("userId") Integer userId) {
+		return userRepository.findUserById(userId).getAddresses();
+	}
 	@GetMapping("/api/addresses")
 	public List<Address> findAllAddresses() {
 		return addressRepository.findAllAddresses();
+	}
+	@GetMapping("/api/addresses/{id}")
+	public Address findAddressById(
+			@PathVariable("id") Integer id) {
+		return addressRepository.findAddressById(id);
 	}
 }
