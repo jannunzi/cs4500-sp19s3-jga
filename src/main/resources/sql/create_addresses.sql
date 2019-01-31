4d69b9fe9d6d611c09af516f33d5edb685a16c7e
jannunzi <jannunzi@gmail.com>
2019-01-30 21:53:38 -0500

    added missing comma

--- src/main/resources/sql/create_addresses.sql
+++ src/main/resources/sql/create_addresses.sql
@@ -6,6 +6,6 @@ create table addresses (
     street1 varchar(255),
     street2 varchar(255),
     user_id integer,
-    primary key (id)
+    primary key (id),
 	constraint FK1fa36y2oqhao3wgg2rw1pi459 foreign key (user_id) references users (id)
 ) engine=InnoDB
